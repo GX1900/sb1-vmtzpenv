102466c088112eb9cf69e4cc72b09392ec2022e2
GX1900 <84753714+GX1900@users.noreply.github.com>
2025-04-07 15:51:25 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-'use client'
+'use client';
 
-export default function HomePage() {
+import { UserButton } from '@clerk/nextjs';
+
+export default function Home() {
   return (
     <div className="min-h-screen p-4 bg-gray-50">
       <div className="flex justify-end">
         <UserButton afterSignOutUrl="/signin" />
       </div>
     </div>
-  )
+  );
 }
 
