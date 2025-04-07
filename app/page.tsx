596fa2597d6a2cc91459fd3e3f838d7a9ea2142a
GX1900 <84753714+GX1900@users.noreply.github.com>
2025-04-07 15:23:39 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 
-import { SignIn } from '@clerk/nextjs'
+import { UserButton } from '@clerk/nextjs'
 
-export const dynamic = 'force-dynamic'
-
-export default function SignInPage() {
-  return <SignIn redirectUrl="/" />
+export default function HomePage() {
+  return (
+    <div className="min-h-screen p-4 bg-gray-50">
+      <div className="flex justify-end">
+        <UserButton afterSignOutUrl="/signin" />
+      </div>
+    </div>
+  )
 }
+
