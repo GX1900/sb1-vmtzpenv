efdc0ec3fe3c8be4b469ffa602a4221256c046db
GX1900 <84753714+GX1900@users.noreply.github.com>
2025-04-07 15:55:32 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 'use client';
 
-import { UserButton } from '@clerk/nextjs';
-
 export default function Home() {
   return (
     <div className="min-h-screen p-4 bg-gray-50">
       <div className="flex justify-end">
-        <UserButton afterSignOutUrl="/signin" />
       </div>
     </div>
   );
