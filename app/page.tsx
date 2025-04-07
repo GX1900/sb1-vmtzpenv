a596bda4b776f4064591a1e2ed7cad1534dd8d25
GX1900 <84753714+GX1900@users.noreply.github.com>
2025-04-07 14:57:19 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,5 +5,5 @@ import { SignIn } from '@clerk/nextjs'
 export const dynamic = 'force-dynamic'
 
 export default function SignInPage() {
-  return <SignIn redirectUrl="/dashboard" />
+  return <SignIn redirectUrl="/" />
 }
