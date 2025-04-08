edf7737e5a22ef47949ba10b8f8ae3ce36f2402f
GX1900 <84753714+GX1900@users.noreply.github.com>
2025-04-08 19:45:05 +0900

    Update page.tsx

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,6 +1,11 @@
-'use client'
-import { SignIn } from '@clerk/nextjs'
+'use client';
 
-export default function SignInPage() {
-  return <SignIn redirectUrl="/" />
+import { SignIn } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <SignIn path="/signin" routing="path" />
+    </div>
+  );
 }
