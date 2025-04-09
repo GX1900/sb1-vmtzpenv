ce66c71dad71754de56100e84fce4fd8bdc6374f
GX1900 <84753714+GX1900@users.noreply.github.com>
2025-04-09 14:14:00 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,14 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publicRoutes: ['/signin', '/sign-up'], // 認証不要なルート
+});
 
 export const config = {
   matcher: [
     '/',
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
+
